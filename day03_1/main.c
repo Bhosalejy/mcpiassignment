f70fd96af8266c45e3d65790a9a30e6daf65604e
E1_Jyoti_83865 <jsbhosale.com>
2024-03-28 18:37:46 +0530

    mcpiassignment

--- day03_1/main.c
+++ day03_1/main.c
@@ -21,39 +21,20 @@
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
 
-#include "led.h"
+#include "led2.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-/*
+
 int main(void)
 {
 	SystemInit();
-	LedInit(LED_BLUE);
+	LedInit(LED_ORANGE);
 	while(1) {
-		LedBlink(LED_BLUE, 500);
+		LedBlink(LED_ORANGE, 500);
 		DelayMs(500);
 	}
 	return 0;
 }
-*/
-
-int main(void)
-{
-	SystemInit();
-	DelayMs(1);
-	LedInit(LED_GREEN);
-	LedInit(LED_ORANGE);
-	LedInit(LED_RED);
-	LedInit(LED_BLUE);
-	while(1) {
-		LedBlink(LED_GREEN, 200);
-		LedBlink(LED_BLUE, 200);
-		LedBlink(LED_RED, 200);
-		LedBlink(LED_ORANGE, 200);
-	}
-	return 0;
-}
-
